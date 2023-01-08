9ddcc52cbac81979d95a36473cf42c8250d041a4
Vardges <vardges.v.margaryan@gmail.com>
2023-01-08 16:34:20 +0400

    Start of Project 2.0

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,21 +9,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView start_logo;
-    Button start_play, start_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start_play = findViewById(R.id.start_play);
-        start_play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, PlayActivity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 }
